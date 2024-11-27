9b68f5cd44ee95aaafc9044e61519b49278d881e
Yaser.ta <yasertarrab10@gmail.com>
2024-11-27 16:57:35 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+let user_Name = "Yaser Tarrab";
+let age=20;
+let isSubscribed=1>0;
+console.log("User Name:",user_Name)
+console.log("Age" ,age)
+console.log("Subscribad",isSubscribed)
+
+/// طباعة القيم بصيغة مختلفة
+
+
+let Name = "Khamzat chimaez";
+let age_=30;
+let is_Subscribed=1<0;
+console.log("User Name:",Name)
+console.log("Age" ,age_)
+console.log("Subscribad",is_Subscribed)
